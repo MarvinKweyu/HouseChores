a99a45fefa46597d5d14a4a80e5750c3c8b9fb47
MarvinKweyu <mkweyu1@gmail.com>
2019-04-08 19:14:09 +0300

    Corrected errors in command line prompt of complete_chores.py.Improved OOp

--- complete_chores.py
+++ complete_chores.py
@@ -5,55 +5,60 @@ import sys
 import getopt
 import getpass
 
+
 def main():
+    'Give options for command line arguments'
 
     try:
-        (opts,args) = getopt.getopt(sys.argv[1:],'hi:o:',['help','input=','output='])
+        (opts, args) = getopt.getopt(sys.argv[1:], 'hi:o:', ['help', 'input=', 'output='])
 
-    except getopt.GetoptError as err: # an option has been passed without a value
+    except getopt.GetoptError as err:  # an option has been passed without a value
         print(err)
         sys.exit(2)
 
     # if options have been passed
     if len(opts) != 0:
         # loop over options:ex,[(i,HouseChores.xlsx),(o,Updated_timetable.xlsx)]
-        for (option,item) in opts:
-            if option in ('h','--help'):
+        for (option, item) in opts:
+            if option in ('-h', '--help'):
                 usage()
                 sys.exit(2)
-            elif option in ('i','input'):
+            elif option in ('-i', 'input'):
                 input_file = item
-            elif option in ('o','output'):
+            elif option in ('-o', 'output'):
                 output_file = item
             else:
                 usage()
                 sys.exit(2)
-    else: #no options have been passed
+    else:  # no options have been passed
         usage()
         sys.exit(2)
-    accept_files(input_file,output_file)
+    accept_files(input_file, output_file)
     return
 
-def accept_files(input_file,output_file):
+
+def accept_files(input_file, output_file):
     '''Accept command line argument files and create new timetable '''
 
-    timetable = HouseChores(input_file,output_file)
+    timetable = HouseChores(input_file, output_file)
     timetable.read_file()
     print("Writing to file...")
-    timetable.write_to_file()
+    new_table = timetable.write_to_file()
     print("Finishing up..")
     sender_email = input("Enter your gmail: ")
     password = getpass.getpass()
-    timetable.notification(sender_email,password)
+    timetable.notification(sender_email, password, new_table)
     print("Done")
 
     return
 
+
 def usage():
     'Display program usage'
 
-    print("python complete_chores.py -i <old_timetable> -o <new_timetable>")
+    print("\npython complete_chores.py -i <old_timetable> -o <new_timetable>")
     print("Where <new_timetable> is a name the user suggests")
+
 
 if __name__ == '__main__':
     main()
--- room.py
+++ room.py
@@ -1,16 +1,16 @@
 #!/bin/python3
 
 import openpyxl
-import os
 import datetime
 
 import yagmail
 import csv
 
+
 class HouseChores(object):
     """make next weeks timetable for roommates with existing timetable"""
 
-    def __init__(self,chores_file,new_table):
+    def __init__(self, chores_file, new_table):
         '''initialize the class '''
         self.chores_file = chores_file
         self.new_table = new_table
@@ -27,12 +27,12 @@ class HouseChores(object):
         self.old_greens = []
         self.old_water = []
 
-        for rowNumber in range(5,self.ws.max_row+1):#skip first five rows
-            self.mopper_name = self.ws.cell(row=rowNumber,column=4).value #mopping
+        for rowNumber in range(5, self.ws.max_row+1):  # skip first five rows
+            self.mopper_name = self.ws.cell(row=rowNumber, column=4).value  # mopping
             self.old_mopping.append(self.mopper_name)
-            self.greens_name = self.ws.cell(row=rowNumber,column=5).value #greens
+            self.greens_name = self.ws.cell(row=rowNumber, column=5).value  # greens
             self.old_greens.append(self.greens_name)
-            self.fetcher_name = self.ws.cell(row=rowNumber,column=8).value #water
+            self.fetcher_name = self.ws.cell(row=rowNumber, column=8).value  # water
             self.old_water.append(self.fetcher_name)
         self.__make_new_names()
 
@@ -44,43 +44,43 @@ class HouseChores(object):
         self.new_greens = []
         self.new_water = []
 
-        #for greens
+        # for greens
         self.new_greens += self.old_greens[-4:]
         self.new_greens += self.old_greens[-4:-1]
 
-        #for mopping
+        # for mopping
         self.new_mopping += self.old_mopping[-4:]
         self.new_mopping += self.old_mopping[-4:-1]
 
-        #for water
+        # for water
         self.new_water += self.old_water[-4:]
         self.new_water += self.old_water[-4:-1]
 
         self.write_to_file()
 
-
     def write_to_file(self):
         "write the new timetable to a file"
 
-        #add duration of timetable
+        # add duration of timetable
         self.today = datetime.date.today()
         self.duration = datetime.timedelta(days=7)
         self.end_date = self.today+self.duration
         self.today = self.today.strftime("%d-%b-%Y")
         self.end_date = self.end_date.strftime("%d-%b-%Y")
-        self.ws.cell(row=2,column=1).value = 'Dated '+str(self.today)+" to "+str(self.end_date)
+        self.ws.cell(row=2, column=1).value = 'Dated '+str(self.today)+" to "+str(self.end_date)
 
         self.item_number = 0
-        for rowNumber in range(5,self.ws.max_row+1):#skip first five rows
-            self.ws.cell(row=rowNumber,column=4).value = self.new_mopping[self.item_number] # column 4 cleaning
-            self.ws.cell(row=rowNumber,column=5).value = self.new_greens[self.item_number] #greens
-            self.ws.cell(row=rowNumber,column=8).value = self.new_water[self.item_number] # water
-            self.item_number +=1
+        for rowNumber in range(5, self.ws.max_row+1):  # skip first five rows
+            self.ws.cell(row=rowNumber, column=4).value = self.new_mopping[self.item_number]  # column 4 cleaning
+            self.ws.cell(row=rowNumber, column=5).value = self.new_greens[self.item_number]  # greens
+            self.ws.cell(row=rowNumber, column=8).value = self.new_water[self.item_number]  # water
+            self.item_number += 1
 
         self.wb.save(self.new_table)
         self.wb.close()
+        return self.new_table
 
-    def notification(self,sender_email,password):
+    def notification(self, sender_email, password, new_table):
         '''Send email notification to members  '''
 
         self.message = """\
@@ -88,13 +88,14 @@ class HouseChores(object):
         """
 
         with open("room.csv") as contacts:
-            self.yag = yagmail.SMTP(sender_email,password)
+            self.yag = yagmail.SMTP(sender_email, password)
             self.reader = csv.reader(contacts)
-            next(self.reader) #skip head
-            for name,email in self.reader:
+            next(self.reader)  # skip head
+            for name, email in self.reader:
                 self.body = self.message.format(name=name)
                 print(f"Sending mail to {name}")
-                self.yag.send(to=email,
+                self.yag.send(
+                         to=email,
                          subject='House chores timetable',
-                         contents=[self.body,self.new_table]
+                         contents=[self.body, new_table]
                         )
